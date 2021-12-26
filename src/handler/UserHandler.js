ff5122f475e163b7369673978fde4d97b83a8a57
yehiafouad <yehiafouad.yf@gmail.com>
2021-12-27 01:07:49 +0200

    initial commit

--- src/handler/UserHandler.js
+++ src/handler/UserHandler.js
@@ -1,6 +1,18 @@
 const { User } = require("../../models");
 
 class UserHandler {
+  // Create User Handler
+  static async createUserHandler(userContent) {
+    const { name, email, phoneNumber } = JSON.parse(userContent);
+
+    const newUser = await User.create({
+      name,
+      email,
+      phoneNumber,
+    });
+
+    return newUser;
+  }
   // Get All Users
   static async getAllUsersHandler(userContent) {
     const users = await User.findAll({
