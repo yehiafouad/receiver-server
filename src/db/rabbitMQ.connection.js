d3a5b769c0660a563103de737bbb3986c65d2fba
yehiafouad <yehiafouad.yf@gmail.com>
2021-12-26 15:28:28 +0200

    inital commit

--- src/db/rabbitMQ.connection.js
+++ src/db/rabbitMQ.connection.js
@@ -2,52 +2,29 @@ const ampq = require("amqplib/callback_api");
 const UserHandler = require("../handler/UserHandler");
 
 // Recieve from rabbitMQ queues and payloads
-ampq.connect("amqp://localhost", (err, connection) => {
-  if (err) {
-    throw err;
-  }
-
-  // Create Channel
-  connection.createChannel(async (chErr, channel) => {
-    if (chErr) {
-      throw chErr;
-    }
-
-    await createUserQueue("createUser", channel);
-
-    process.on("beforeExit", () => {
-      connection.close();
-    });
-  });
-});
-
-// Send To Queue
-const sendToQueue = (queueName, data) => {
-  ampq.connect("amqp://localhost", (err, connection) => {
+ampq.connect(
+  process.env.NODE_ENV === "development"
+    ? "amqp://localhost"
+    : process.env.RABBITMQ_URL,
+  (err, connection) => {
     if (err) {
       throw err;
     }
 
     // Create Channel
-    connection.createChannel((chErr, channel) => {
+    connection.createChannel(async (chErr, channel) => {
       if (chErr) {
         throw chErr;
       }
 
-      // Assert Queue
-      const QUEUE = queueName;
-      channel.assertQueue(QUEUE, { durable: false });
+      await createUserQueue("createUser", channel);
 
-      // Send Message to queue
-      channel.sendToQueue(QUEUE, Buffer.from(JSON.stringify(data)));
-      console.log("Message Send");
+      process.on("beforeExit", () => {
+        connection.close();
+      });
     });
-
-    // process.on("beforeExit", () => {
-    //   connection.close();
-    // });
-  });
-};
+  }
+);
 
 // Create User
 const createUserQueue = async (queueName, channel) => {
@@ -56,10 +33,7 @@ const createUserQueue = async (queueName, channel) => {
   channel.consume(
     queueName,
     async (msg) => {
-      const newUser = await UserHandler.createUserHandler(
-        JSON.parse(msg.content.toString())
-      );
-      // sendToQueue(queueName + "-recieve", newUser);
+      await UserHandler.createUserHandler(JSON.parse(msg.content.toString()));
     },
     { noAck: true }
   );
